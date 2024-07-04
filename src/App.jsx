ec6150c6e8b6d03f11cc37e8783ee73de9487564
Jesus Fajardo <es.jfajardobass@gmail.com>
2024-07-04 16:06:45 +0200

    Initial commit / Login form

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import 'primereact/resources/themes/arya-green/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeflex/primeflex.css'
+import 'primeicons/primeicons.css'
+
+import Login from './components/Login.jsx';
+
+function App() {
+    return (
+        <>
+            <div className="App">
+                <Login />
+            </div>
+        </>
+    );
+}
+
+export default App;
